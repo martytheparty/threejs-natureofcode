b02bec6e71b2cd504be41a4e5b78564ff3add52d
martytheparty <marty@ilikeemail.com>
2018-12-30 20:56:29 -0500

    Adds offsets to the perlin noise.  This causes the spheres to start at the origin.

---
+++ noc/spheres/006perlin-paced-offset/index.js
@@ -0,0 +1,97 @@
+let camera, scene, renderer, geometry, material;
+let framerate = 60;
+let meshes = [];
+let positions = [];
+let count = 1;
+
+
+function setup() {
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 4000 );
+    camera.position.z = 20;
+    //camera.position.y = 80;
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    /* Add A Sphere To To Canvas */
+    geometry = new THREE.SphereGeometry( 1, 32, 32 );
+    //geometry = new THREE.BoxGeometry( 10, 10, 10 );
+    material = new THREE.MeshNormalMaterial();
+
+
+    for(let i = 0; i < count; i++) {
+      meshes[i] = new THREE.Mesh( geometry, material );
+      meshes[i].xPosition = 0;
+      meshes[i].yPosition = i*2 - 10;
+      meshes[i].perlinXOffset = false;
+      meshes[i].perlinYOffset = false;
+      scene.add( meshes[i] );
+    }
+
+    draw();
+}
+
+let currentCount = 0;
+const setNextRandom = () => {
+
+  currentCount = currentCount + .01;
+
+  meshes.forEach(
+    (mesh, i) => {
+      mesh.xPosition = Simplex.noise(i, currentCount)*20;
+      mesh.yPosition = Simplex.noise(i+count, currentCount)*20;
+
+      if (meshes[i].perlinXOffset === false) {
+        mesh.perlinXOffset = mesh.xPosition;
+        mesh.perlinYOffset = mesh.yPosition;
+      }
+
+    }
+  );
+
+  //return val;
+};
+
+const updateSceneData = () => {
+  setNextRandom();
+}
+
+let sceneUpdateInterval = setInterval(updateSceneData, 1000/framerate);
+
+setInterval(() => {
+
+  if (meshes.length < 50) {
+    meshes[meshes.length] = new THREE.Mesh( geometry, material );
+    scene.add( meshes[meshes.length - 1] );
+    meshes[meshes.length - 1].perlinXOffset = false;
+    meshes[meshes.length - 1].perlinYOffset = false;
+  }
+
+
+}, 500);
+
+const updateFramerate = (newFrameRate) => {
+  framerate = newFrameRate;
+  clearInterval(sceneUpdateInterval);
+  sceneUpdateInterval = setInterval(updateSceneData, 1000/framerate);
+}
+
+function draw() {
+    /*
+    This function call called on every browser render (requestAnimationFrame) event.
+    */
+
+    meshes.forEach(
+      (mesh) => {
+        mesh.position.set(mesh.xPosition - mesh.perlinXOffset, mesh.yPosition  - mesh.perlinYOffset, 0);
+      }
+    );
+
+
+    renderer.render( scene, camera );
+    requestAnimationFrame( draw );
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  setup();
+});
