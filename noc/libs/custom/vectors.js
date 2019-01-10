fbddc3a3d1f075787cdeb158e20627ac0637fbca
martytheparty <marty@ilikeemail.com>
2019-01-09 21:42:20 -0500

    adds subtract and clone to vector

--- noc/libs/custom/vectors.js
+++ noc/libs/custom/vectors.js
@@ -10,6 +10,16 @@ class MVector {
     this.y = this.y + vector.y;
     this.z = this.z + vector.z;
   }
+  substract(vector) {
+    this.x = this.x - vector.x;
+    this.y = this.y - vector.y;
+    this.z = this.z - vector.z;
+  }
+  clone(vector) {
+    const myVec = new MVector(this.x, this.y, this.z);
+    myVec.magnitude = this.magnitude;
+    return myVec;
+  }
   setMagnitute(magnitude) {
     this.magnitude = magnitude;
   }
