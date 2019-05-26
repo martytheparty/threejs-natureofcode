f06950080a8d9d684c97a574d44d9830f71a8f3b
martytheparty <marty@ilikeemail.com>
2019-05-26 15:24:18 -0400

    Starts building a new library that creates cannonjs/3js objects.

---
+++ noc/libs/custom/makers.js
@@ -0,0 +1,19 @@
+/*
+* The idea of this library is to make functions that return objects that
+* have:
+* 1. General Attributes of objects
+* 2. A reference to an object in CannonJS Physics Engine World
+* 3. A rererence to an object in a 3JS Scene
+*
+* NOT IN THIS LIBRARY:
+* - a list of objects
+* - interaction logic between objects
+*
+* TO SUM UP:
+* Basic utility like functions that accept argurment and return objects is all
+* that should ever exist in this file.
+*/
+
+const fixedCuboid() {
+  /* This is something like a ground or a building... ie something that doesn't move */
+}
