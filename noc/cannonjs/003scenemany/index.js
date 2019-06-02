6e62557ac1498841c4d7ae39778474fb24b0ce5d
martytheparty <marty@ilikeemail.com>
2019-06-01 22:26:33 -0400

    Adds static cube examples.

---
+++ noc/cannonjs/003scenemany/index.js
@@ -0,0 +1,223 @@
+let world, body, shape, timeStep=1/60,
+camera, scene, renderer, geometry, material, mesh;
+
+let world2, body2, shape2, camera2, scene2, renderer2, geometry2, material2, mesh2;
+
+let framerate = 60;
+let cannonDebugRenderer = new THREE.CannonDebugRenderer( scene, world );
+let mass = 5, radius = 1;
+let cannonElement;
+let threeElement;
+let theta = Math.PI;
+let controls;
+let ground;
+
+
+function setupCamera1() {
+  camera = new THREE.PerspectiveCamera( 70, 1, 0.01, 200 );
+  camera.position.x = 20;
+  camera.position.y = 30;
+  //camera.position.x = Math.cos(theta)*20;
+  camera.lookAt(new THREE.Vector3(0,0,0));
+}
+
+function setupCamera2() {
+  camera2 = new THREE.PerspectiveCamera( 70, 1, 0.01, 200 );
+  controls = new THREE.OrbitControls( camera2 );
+  //camera2.position.z = Math.sin(theta)*20;
+  camera2.position.x = 20;
+  camera2.position.y = 30;
+  camera2.lookAt(new THREE.Vector3(0,0,0));
+  controls.update();
+
+}
+
+function setupWorld1() {
+  world = new CANNON.World();
+  world.gravity.set(0,0,-10);
+  world.broadphase = new CANNON.NaiveBroadphase();
+  world.solver.iterations = 10;
+}
+
+function setupWorld2() {
+  world2 = new CANNON.World();
+  world2.gravity.set(0,0,-10);
+  world2.broadphase = new CANNON.NaiveBroadphase();
+  world2.solver.iterations = 10;
+}
+
+function setupRenderer1() {
+  scene = new THREE.Scene();
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setSize( cannon.offsetWidth, cannon.offsetHeight );
+  cannonElement.appendChild( renderer.domElement );
+}
+
+function setupRenderer2() {
+  scene2 = new THREE.Scene();
+  renderer2 = new THREE.WebGLRenderer( { antialias: true } );
+  renderer2.setSize( threeElement.offsetWidth, threeElement.offsetHeight );
+  threeElement.appendChild( renderer2.domElement );
+}
+
+
+
+function draw1() {
+  //theta = theta + .05;
+  camera.position.z = Math.sin(theta)*20;
+  camera.position.x = Math.cos(theta)*20;
+  camera.lookAt(new THREE.Vector3(0,0,0));
+  cannonDebugRenderer.update();
+  renderer.render( scene, camera );
+  requestAnimationFrame( draw1 );
+}
+
+function draw2() {
+  camera2.position.z = Math.sin(theta)*20;
+  camera2.position.x = Math.cos(theta)*20;
+  camera2.lookAt(new THREE.Vector3(0,0,0));
+  renderer2.render( scene2, camera2 );
+  controls.update();
+  requestAnimationFrame( draw2 );
+}
+
+
+
+
+
+
+function setup1() {
+  cannonElement = document.getElementById("cannon");
+  setupCamera1();
+  setupWorld1();
+  setupRenderer1();
+  cannonDebugRenderer = new THREE.CannonDebugRenderer( scene, world );
+  draw1();
+}
+
+function setup2() {
+  threeElement = document.getElementById("cube");
+  setupCamera2();
+  setupWorld2();
+  setupRenderer2();
+  draw2();
+}
+
+const updateSceneData = () => {
+  updatePhysics();
+}
+
+const updateFramerate = (newFrameRate) => {
+  framerate = newFrameRate;
+  clearInterval(sceneUpdateInterval);
+  sceneUpdateInterval = setInterval(updateSceneData, 1000/framerate);
+}
+
+function updatePhysics() {
+    world.step(timeStep);
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  setup1();
+  setup2();
+
+  // cuboid descriptor
+  // {
+  //   position: { x: number, y: number, z: number },
+  //   rotation: { x: number, y: number, z: number },
+  //   dimensions { width: number, height: number, depth: number },
+  //   scene: threeJsScene,
+  //   world; cannonJsWorld,
+  //   debugWorld: debugWorld
+  // }
+  let options = {};
+  options.position = {x: 0, y: -5, z: 0};
+  options.rotation = {x: -1*Math.PI/16, y: 0, z: 1*Math.PI/32};
+  options.dimensions = {width: 8, height: .1, depth: 8};
+  options.scene = scene2;
+  options.world = world2;
+  options.debugWorld = world;
+
+  ground = fixedCuboid(options);
+
+
+  let options1 = {};
+  options1.position = {x: 0, y: 5, z: 0};
+  options1.rotation = {x: Math.PI/8, y: 0, z: Math.PI/8};
+  options1.dimensions = {width: 3, height: .1, depth: 3};
+  options1.scene = scene2;
+  options1.world = world2;
+  options1.debugWorld = world;
+
+  ground1 = fixedCuboid(options1);
+
+  let options2 = {};
+  options2.position = {x: 0, y: 8, z: 0};
+  options2.rotation = {x: -1*Math.PI/8, y: 0, z: Math.PI/8};
+  options2.dimensions = {width: 1, height: .1, depth: 1};
+  options2.scene = scene2;
+  options2.world = world2;
+  options2.debugWorld = world;
+
+  ground2 = fixedCuboid(options2);
+
+  let toggle1 = 1;
+  let toggle2 = 1;
+  for(let i = 0; i < 22; i++) {
+    // let opt = {};
+    // opt.position = {x: 0, y: i, z: i};
+    // opt.rotation = {x: 0, y: 0, z: 0};
+    // opt.dimensions = {width: 1, height: .1, depth: 1};
+    // opt.scene = scene2;
+    // opt.world = world2;
+    // opt.debugWorld = world;
+    // fixedCuboid(opt);
+    for(let j = 0; j < 4; j++) {
+      // let opt = {};
+      // opt.position = {x: 0, y: i, z: j};
+      // opt.rotation = {x: 0, y: 0, z: 0};
+      // opt.dimensions = {width: 1, height: .1, depth: 1};
+      // opt.scene = scene2;
+      // opt.world = world2;
+      // opt.debugWorld = world;
+      // fixedCuboid(opt);
+      for(let k = 0; k < 5; k++) {
+
+
+        if (toggle1 === 1 && toggle2 === 1) {
+          toggle1 = 1;
+          toggle2 = -1;
+        } else if (toggle1 === 1 && toggle2 === -1) {
+          toggle1 = -1;
+          toggle2 = 1;
+        } else if (toggle1 === -1 && toggle2 === 1) {
+          toggle1 = -1;
+          toggle2 = -1;
+        } else if (toggle1 === -1 && toggle2 === -1) {
+          toggle1 = 1;
+          toggle2 = 1;
+        }
+
+        let opt = {};
+        opt.position = {x: Math.sin(i*.3)*j*5, y: 3*j, z: Math.cos(i*.3)*j*5};
+        opt.rotation = {x: 1*Math.PI/8, y: 1*Math.PI/8, z: -1*Math.PI/4};
+        opt.dimensions = {width: 1, height: .1, depth: 1};
+        opt.scene = scene2;
+        opt.world = world2;
+        opt.debugWorld = world;
+        fixedCuboid(opt);
+
+      }
+    }
+  }
+
+
+
+
+  document.onclick = () => {
+    //addSphere();
+  }
+});
+sceneUpdateInterval = setInterval(updateSceneData, 1000/framerate);
--- noc/libs/custom/makers.js
+++ noc/libs/custom/makers.js
@@ -14,6 +14,59 @@
 * that should ever exist in this file.
 */
 
-const fixedCuboid() {
+/*
+
+cuboid descriptor
+{
+  position: { x: number, y: number, z: number },
+  rotation: { x: number, y: number, z: number },
+  dimensions { width: number, height: number, depth: number },
+  scene: threeJsScene,
+  world; cannonJsWorld,
+  debugWorld: debugWorld
+}
+
+*/
+
+const fixedCuboid = (descriptor) => {
   /* This is something like a ground or a building... ie something that doesn't move */
+
+  // function addBox1() {
+  //   var shape = new CANNON.Box(new CANNON.Vec3(radius,radius,radius));
+  //   var body = new CANNON.Body({ mass: 0 });
+  //   body.addShape(shape);
+  //   body.position.set(0,0,0);
+  //   world.addBody(body);
+  // }
+  //
+  // function addBox2() {
+  //   var shape = new THREE.BoxGeometry( radius*2, radius*2, radius*2 );
+  //   var mat = new THREE.MeshNormalMaterial();
+  //   var mesh = new THREE.Mesh( shape, mat );
+  //   mesh.position.set(0, 0, 0);
+  //   scene2.add( mesh );
+  // }
+
+
+  // body.quaternion.setFromEuler(Math.PI/2, 0, 0);
+  let cannonShape = new CANNON.Box(new CANNON.Vec3(descriptor.dimensions.depth, descriptor.dimensions.height, descriptor.dimensions.width));
+  let cannonBody = new CANNON.Body({ mass: 0 });
+  cannonBody.addShape(cannonShape);
+  cannonBody.position.set(descriptor.position.x,descriptor.position.y,descriptor.position.z);
+  cannonBody.quaternion.setFromEuler(descriptor.rotation.x, descriptor.rotation.y, descriptor.rotation.z);
+  descriptor.debugWorld.addBody(cannonBody);
+
+  let threeShape = new THREE.BoxGeometry( descriptor.dimensions.width*2,descriptor.dimensions.height*2,descriptor.dimensions.depth*2 );
+  let mat = new THREE.MeshNormalMaterial();
+  let threeMesh = new THREE.Mesh( threeShape, mat );
+  threeMesh.position.set(-1*descriptor.position.x,descriptor.position.y,-1*descriptor.position.z);
+  threeMesh.rotation.x = descriptor.rotation.x;
+  threeMesh.rotation.y = descriptor.rotation.y;
+  threeMesh.rotation.z = descriptor.rotation.z;
+
+  scene2.add( threeMesh );
+
+
+  return { cannon: cannonShape, three: threeMesh };
+
 }
