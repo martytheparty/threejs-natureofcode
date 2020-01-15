cb1f553c4d3a3d0484d702b9c30f460d8e3c6e5b
martytheparty <marty@ilikeemail.com>
2020-01-14 19:07:54 -0500

    Adds physics.

--- games/labyrinth/game.js
+++ games/labyrinth/game.js
@@ -1,19 +1,31 @@
 ( () => {
+    let world;
+    const floor = new ThreeDObject();
+
     const platformDescription = {
         width: 150,
         height: 150,
         depth: 15,
         x: 0,
         y: 0,
-        x: 0
+        z: 0
     };
     const sphereDescription = {
-        radius: 10,
+        radius: 20,
         sides: 32,
-        x: -60, 
+        x: -150, 
         y: 0, 
-        z: 15
+        z: 50
     };
+
+    const sphereMeshes = [];
+    const plaformMeshes = [];
+    const gravity = -100;
+    let topCameraPosition = 350;
+    let frontCameraXPosition = 250;
+    let frontCameraZPosition = 20;
+    let leftCameraYPosition = -250;
+    let leftCameraZPosition = 20;
 
     const view = {
         createMainView: (mesh, sphereMesh) => {
@@ -24,7 +36,7 @@
                 camera: new THREE.PerspectiveCamera(75, mainElement.width / mainElement.height, 0.1, 1000),
                 renderer: new THREE.WebGLRenderer({ antialias: true, canvas: mainElement })
             }
-            threeObject.camera.position.set(0, 0, 150);
+            threeObject.camera.position.set(0, 0, topCameraPosition);
             threeObject.camera.up.set(0, 0, 1);
             threeObject.camera.lookAt(new THREE.Vector3(0, 0, 0));
             threeObject.scene.add(mesh);
@@ -44,9 +56,9 @@
                 renderer: new THREE.WebGLRenderer({ antialias: true, canvas: frontElement })
             }
             threeObject.camera.up.set(0, 0, 1);
-            threeObject.camera.position.x = 125;
+            threeObject.camera.position.x = frontCameraXPosition;
             threeObject.camera.position.y = 0;
-            threeObject.camera.position.z = 80;
+            threeObject.camera.position.z = frontCameraZPosition;
             threeObject.camera.lookAt(new THREE.Vector3(0, 0, 0));
             threeObject.scene.add(mesh);
             if (sphereMesh) {
@@ -67,8 +79,8 @@
             }
             threeObject.camera.up.set(0, 0, 1);
             threeObject.camera.position.x = 0;
-            threeObject.camera.position.y = -125;
-            threeObject.camera.position.z = 80;
+            threeObject.camera.position.y = leftCameraYPosition;
+            threeObject.camera.position.z = leftCameraZPosition;
             threeObject.camera.lookAt(new THREE.Vector3(0, 0, 0));
             threeObject.scene.add(mesh);
             if (sphereMesh) {
@@ -104,48 +116,155 @@
             return mesh;
         },
         createViews: (meshDescription, sphereMeshDescription) => {
+            sphereMeshes.push(view.getRenderingMesh(sphereMeshDescription));
+            sphereMeshes.push(view.getRenderingMesh(sphereMeshDescription));
+            sphereMeshes.push(view.getRenderingMesh(sphereMeshDescription));
+            sphereMeshes.push(view.getRenderingMesh(sphereMeshDescription));
+
+            plaformMeshes.push(view.getRenderingMesh(meshDescription));
+            plaformMeshes.push(view.getRenderingMesh(meshDescription));
+            plaformMeshes.push(view.getRenderingMesh(meshDescription));
+            plaformMeshes.push(view.getRenderingMesh(meshDescription));
+
             const views = [];
-            views.push(view.createMainView(view.getRenderingMesh(meshDescription), view.getRenderingMesh(sphereMeshDescription)));
-            views.push(view.createFrontView(view.getRenderingMesh(meshDescription), view.getRenderingMesh(sphereMeshDescription)));
-            views.push(view.createLeftView(view.getRenderingMesh(meshDescription), view.getRenderingMesh(sphereMeshDescription)));
-            views.push(view.createBallView(view.getRenderingMesh(meshDescription), view.getRenderingMesh(sphereMeshDescription)));
+            views.push(view.createMainView(plaformMeshes[0], sphereMeshes[0]));
+            views.push(view.createFrontView(plaformMeshes[1], sphereMeshes[1]));
+            views.push(view.createLeftView(plaformMeshes[2], sphereMeshes[2]));
+            views.push(view.createBallView(plaformMeshes[3], sphereMeshes[3]));
             return views;
         }
     }
 
-    function createRenderingLayer(hasSphere) {
-        const floor = new ThreeDObject();
-        const geometry = new THREE.BoxGeometry(platformDescription.width, platformDescription.height, platformDescription.depth);
+    const physics = {
+        createWorld : () => {
+            world = new CANNON.World();
+            world.gravity.set(0,0,gravity);
+            world.broadphase = new CANNON.NaiveBroadphase();
+            world.solver.iterations = 100;
+            world.solver.tolerance = 0;
+        },
+        addShapes: () => {
+            const material = new CANNON.Material("material");
+            const gg = {
+                friction: 0.4,
+                restitution: 0.1,
+                contactEquationStiffness: 1e8,
+                contactEquationRelaxation: 3,
+                frictionEquationStiffness: 1e8,
+                frictionEquationRegularizationTime: 3
+              };
+            const groundGroundContact = new CANNON.ContactMaterial(material, material, gg);
+            const cannonSphere = new CANNON.Sphere(
+                sphereDescription.radius
+            );
+            const cannonBody = new CANNON.Body({ mass: 10000 }, material);
+    
+    
+            cannonBody.addShape(cannonSphere);
+            cannonBody.position.set(
+                sphereDescription.x,
+                sphereDescription.y,
+                sphereDescription.z
+            );
+            world.add(cannonBody);
+    
+            let cannonShape = new CANNON.Box(new CANNON.Vec3(platformDescription.width, platformDescription.height, platformDescription.depth));
+            let cannonBody1 = new CANNON.Body({ mass: 0 });
+            cannonBody1.addShape(cannonShape);
+            cannonBody1.position.set(
+                platformDescription.x,
+                platformDescription.y,
+                platformDescription.z-platformDescription.depth/2
+            );
+            world.add(cannonBody1);
+    
+            let cannonBody2 = new CANNON.Body({ mass: 0 });
+            cannonBody2.addShape(cannonShape);
+            cannonBody2.position.set(
+                platformDescription.x,
+                platformDescription.y,
+                platformDescription.z-(platformDescription.depth + platformDescription.depth/2)
+            );
+            world.add(cannonBody2);
+    
+            floor.sphereLookup.cannonIndex = floor.cannonObjects.length;
+            floor.cannonObjects.push(cannonBody);
+            floor.platformLookup.uiPlatformIndex = floor.cannonObjects.length;
+            floor.cannonObjects.push(cannonBody1);
+            floor.platformLookup.rotatePlatformIndex = floor.cannonObjects.length;
+            floor.cannonObjects.push(cannonBody2);
+        }
+
+    }
+
+    function setView() {
+        const geometry = new THREE.BoxGeometry(platformDescription.width*2, platformDescription.height*2, platformDescription.depth*2);
         const material = new THREE.MeshBasicMaterial({ color: 0xC19A6B });
         const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xFFFFFF });
         const meshDescription = { geometry, material, x: 0, y: 0, z: 0 };
         sphereGeometry = new THREE.SphereGeometry(sphereDescription.radius, sphereDescription.sides, sphereDescription.sides);
         const sphereMeshDescription = { geometry: sphereGeometry, material: sphereMaterial, x: sphereDescription.x, y: sphereDescription.y, z: sphereDescription.z };    
-        floor.threeObjects = view.createViews(meshDescription, sphereMeshDescription);
-        return floor;
+        floor.sphereLookup.threeIndex = floor.threeObjects.length;
+        floor.threeObjects.push(sphereGeometry);
+        floor.threeObjects.push(meshDescription);
+        floor.layers = view.createViews(meshDescription, sphereMeshDescription);
     }
 
-    function createFloor(hasSphere) {
-        let floor = createRenderingLayer(hasSphere);
-        return floor;
+    function setPhysics() {
+        physics.createWorld();
+        physics.addShapes();
+    }
+
+    function setupFloor() {
+        setView();
+        setPhysics();
+        // console.log('setup');
+        // console.log(plaformMeshes);
+        // plaformMeshes.forEach(
+        //     (mesh) => {
+        //       mesh.rotation.x = Math.PI / 2;
+        //     }
+        // );
+
     }
 
     function animate() {
         requestAnimationFrame(animate);
         layers.forEach(
             (layer, index) => {
-                layer.threeObjects.forEach(
+                layer.layers.forEach(
                     (threeObject) => {
                         threeObject.renderer.render(threeObject.scene, threeObject.camera);
                     }
                 );
             }
         );
-
     }
 
+    const calculatePhysics = () => {
+        if (floor.cannonObjects && floor.cannonObjects.length > 0) {
+            world.step(1/30);
+            const sphereBody = floor.cannonObjects[floor.sphereLookup.cannonIndex];
+
+            sphereMeshes.forEach(
+                (sphere) => {
+                    sphere.position.x = sphereBody.position.x;
+                    sphere.position.y = sphereBody.position.y;
+                    sphere.position.z = sphereBody.position.z;
+
+                    sphere.quaternion.x = sphereBody.quaternion.x;
+                    sphere.quaternion.y = sphereBody.quaternion.y;
+                    sphere.quaternion.z = sphereBody.quaternion.z;
+                    sphere.quaternion.w = sphereBody.quaternion.w;
+                }
+            );
+        }
+    }
+
+    setupFloor();
     let layers = [];
-    layers.push(createFloor(true));
+    layers.push(floor);
     animate();
+    setInterval(calculatePhysics, 10);
  }
 )()
--- games/libs/custom/objects.js
+++ games/libs/custom/objects.js
@@ -1,5 +1,15 @@
 function ThreeDObject() {
+    this.layers = [];
     this.threeObjects = [];
     this.cannonObjects = [];
+    this.sphereLookup = {
+        threeIndex: 0,
+        cannonIndex: 0
+    };
+    this.platformLookup = {
+        uiPlatformIndex: 1,
+        rotatePlatformIndex: 2
+    };
+
     return this;
 } 
